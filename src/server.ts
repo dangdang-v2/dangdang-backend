1144f27534e18d1dc3019171b7a6983a9bbab75e
Gitakkkk <stom1028@naver.com>
2023-02-02 02:44:10 +0900

    refactor: main file to open the server.

--- src/server.ts
+++ src/server.ts
@@ -1,73 +1,31 @@
 import express from 'express';
 import connect from './schemas/index';
 import cors from 'cors';
-const app = express();
 import passport from 'passport';
 import passportConfig from './passport';
-const port = process.env.PORT;
 import rateLimit from 'express-rate-limit';
 import helmet from 'helmet';
-import ms from 'ms';
 import userRouter from './routes/user';
 import guideRouter from './routes/guide';
 import profileRouter from './routes/profile';
 import mapRouter from './routes/map';
 import emailRouter from './routes/email';
+const app = express();
+const port = process.env.PORT;
 require('dotenv').config();
-
-const options = {
-  ca: fs.readFileSync('/etc/letsencrypt/live/dengroundserver.com/fullchain.pem'),
-  key: fs.readFileSync('/etc/letsencrypt/live/dengroundserver.com/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/dengroundserver.com/cert.pem'),
-};
-
-https.createServer(options, app).listen(443, () => {
-  console.log(port, '포트로 서버가 켜졌습니다.');
-});
 
 connect();
 passportConfig();
 
-app.use((req, res, next) => {
-  console.log('Request URL:', `[${req.method}]`, req.originalUrl, ' - ', new Date().toLocaleString());
-  next();
-});
-
-app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.disable('x-powered-by');
 
-app.use(
-  cors({
-    origin: 'https://denground.com',
-    credentials: true,
-  }),
-);
-
-app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(
-  session({
-    resave: true,
-    saveUninitialized: true,
-    secret: process.env.COOKIE_SECRET,
-    cookie: {
-      httpOnly: true,
-      secure: true,
-    },
-  }),
-);
-
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(
-  helmet.hsts({
-    maxAge: ms('1 year'),
-    includeSubDomains: true,
-  }),
-);
+app.use(cors());
 
 app.use(
   rateLimit({
@@ -77,9 +35,11 @@ app.use(
 );
 
 app.use(helmet.xssFilter());
-
-app.use(helmet.frameguard('deny'));
-
+app.use(helmet.frameguard({ action: 'deny' }));
 app.use(helmet.noSniff());
 
 app.use('/api', [userRouter, guideRouter, profileRouter, mapRouter, emailRouter]);
+
+app.listen(port, () => {
+  console.log('server open');
+});
